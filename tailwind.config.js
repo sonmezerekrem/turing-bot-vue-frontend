f39183e272e59822f7d9cbdb6b3a9f0105f7b959
Ekrem Sönmezer <ekremsonmezer@gmail.com>
2021-06-21 21:48:56 +0300

    basic configuration is done

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    purge: [
+        './src/**/*.html',
+        './src/**/*.vue',
+        './src/**/*.jsx',
+    ],
+    darkMode: "media", // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
